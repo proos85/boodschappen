c3a5e05f4ec238be264bbadda47d9d7fa766d048
Patrick Roos <pinguin.power@gmail.com>
2019-06-22 14:28:30 +0200

    * .gitignore: Initial setup
    * Boodschappen.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * AppShell.xaml:
    * Info.plist:
    * Item.cs:
    * AssemblyInfo.cs:
    * AppShell.xaml.cs:
    * AppDelegate.cs:
    * Boodschappen.csproj:
    * AboutPage.xaml:
    * ItemsPage.xaml:
    * NewItemPage.xaml:
    * IDataStore.cs:
    * Entitlements.plist:
    * AboutPage.xaml.cs:
    * ItemsPage.xaml.cs:
    * MockDataStore.cs:
    * ItemDetailPage.xaml:
    * Default.png:
    * NewItemPage.xaml.cs:
    * tab_feed.png:
    * BaseViewModel.cs:
    * tab_about.png:
    * Boodschappen.iOS.csproj:
    * AboutViewModel.cs:
    * Default@2x.png:
    * ItemDetailPage.xaml.cs:
    * ItemsViewModel.cs:
    * tab_feed@3x.png:
    * tab_feed@2x.png:
    * xamarin_logo.png:
    * AssemblyInfo.cs:
    * tab_about@2x.png:
    * tab_about@3x.png:
    * xamarin_logo@2x.png:
    * xamarin_logo@3x.png:
    * ItemDetailViewModel.cs:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * Icon29.png:
    * Icon20.png:
    * Icon58.png:
    * Icon76.png:
    * Icon80.png:
    * Icon87.png:
    * Icon40.png:
    * Icon60.png:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:

---
+++ Boodschappen/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Boodschappen.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
